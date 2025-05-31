ba11e8267cd61827550a3c4fb19a79bc81ac06d2
Eugene Maximenko <zhek4770@gmail.com>
2025-05-31 15:54:24 +0200

    Add navbar

--- server-config.js
+++ server-config.js
@@ -1,14 +1,17 @@
 const exphbs = require('express-handlebars')
+const express = require('express')
 
-function configureTemplateEngine(application) {
+function configureTemplateEngine(app) {
     const hbs = exphbs.create({
         defaultLayout: 'main',  // Wrap for the target hbs
         extname: 'hbs'
     })
 
-    application.engine('hbs', hbs.engine)   // Register the engine in the system
-    application.set('view engine', 'hbs')   // Use the engine
-    application.set('views', 'views')       // Directory with templates
+    app.engine('hbs', hbs.engine)   // Register the engine in the system
+    app.set('view engine', 'hbs')   // Use the engine
+    app.set('views', 'views')       // Directory with templates
+
+    app.use(express.static('public')) // Make files in the "public" folder accessible via browser (e.g., CSS, images, JS)
 }
 
 const PORT = process.env.PORT || 3000
